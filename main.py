a31cb0d105ec3586cbf2a47c9a5610ed56a202a1
Raindeero <raindeero@mail.ru>
2024-02-02 11:45:08 +0500

    fixed usege examples

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 from linked_list import LinkedList
 
-# llist = LinkedList()
-# llist.head = Node("Monday")
-# second_element = Node("Tuesday")
-# third_element = Node("Wednesday")
-
-# llist.head.next = second_element
-# second_element.next = third_element
-
-# llist.add_to_start("Thursday")
-# llist.add_to_end("Friday")
-# llist.list_print()
-# llist.del_element_by_data("Thursday")
-# llist.del_element_by_data("Friday")
-# llist.list_print()
-# llist.add_element_after("Tuesday", "Friday")
-# llist.list_print()
-# llist.del_list()
-# llist.list_print()
-
+# Create a linked list
 linked_list = LinkedList()
+
+# Add elements to the list
 linked_list.add_to_end(1)
 linked_list.add_to_end(2)
 linked_list.add_to_end(3)
 
-# Сохранение списка в файл
+# Print the original list
+print("Original List:")
+linked_list.list_print()
+
+# Add an element to the start
+linked_list.add_to_start(0)
+
+# Add an element after a specific value
+linked_list.add_element_after(2, 2.5)
+
+# Print the modified list
+print("\nModified List:")
+linked_list.list_print()
+
+# Delete an element by data
+linked_list.del_element_by_data(1)
+
+# Print the list after deletion
+print("\nList after Deletion:")
+linked_list.list_print()
+
+# Save the list to a file
 linked_list.save_list_to_file("linked_list.txt")
 
-# Создание нового списка и загрузка из файла
+# Load the list from a file
 loaded_list = LinkedList()
 loaded_list = loaded_list.load_list_from_file("linked_list.txt")
 
-# Вывод списка после загрузки
+# Print the loaded list
+print("\nLoaded List:")
 loaded_list.list_print()
